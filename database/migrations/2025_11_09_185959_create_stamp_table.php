4d05a198fb899bfb69fbba9b4eae3f5de6382d93
Cazacu Ion <cazacioc51@gmail.com>
2025-11-13 14:14:47 +0200

    Creating of Application Database Migrations
    * database migration

---
+++ database/migrations/2025_11_09_185959_create_stamp_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stamp', function (Blueprint $table) {
+            $table->id('stamp_id');
+            $table->foreignId('company_id')->constrained(table:'company', column:'company_id')->onDelete('cascade');
+            $table->foreignId('student_id')->constrained(table:'student', column:'student_id')->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('consumed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stamp');
+    }
+};
