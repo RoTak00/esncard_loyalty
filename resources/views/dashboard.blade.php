91259c077edf12ed0159a272bfecd4f350149d7f
Cazacu Ion <cazacioc51@gmail.com>
2025-11-18 14:08:03 +0200

    qr-code generator

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard!')}}
+            {{ __('Dashboard!') }}
         </h2>
     </x-slot>
 
     @auth('student')
-    <p>Hello, {{ auth('student')->user()->forename }} {{ auth('student')->user()->surname }}</p>
+        <p>Hello, {{ auth('student')->user()->forename }} {{ auth('student')->user()->surname }}</p>
 
-    <h3>Here's your QR!</h3>
+        <h3>Here's your QR!</h3>
+        <div class="qr-container">
+            {!! QrCode::size(300)->generate(auth('student')->user()->student_id) !!}
+
+        </div>
     @endauth
 
 </x-app-layout>
